bc12689547c6e29dfe1b50775c0c704c44b38545
Subham Rakshit <developersubham@gmail.com>
2017-05-22 20:54:16 +0100

    Added Janusgraph server config

--- janusgraph-properties.sh
+++ janusgraph-properties.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-#set cassandra cassandra.yaml
-sed -i -e "s|^cluster_name:.*$|cluster_name: 'DEV Cassandra Cluster'|" $CASSANDRA_HOME/conf/cassandra.yaml
-sed -i -e "s|^listen_address:.*$|listen_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
-sed -i -e "s|^rpc_address:.*$|rpc_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
-sed -ri "s/(- seeds:).*/\1 \"`hostname -i`\"/" $CASSANDRA_HOME/conf/cassandra.yaml
+#set janusgraph properties
+sed -i -e "s|^storage.backend=.*$|storage.backend=cassandra|" $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
+sed -i -e "s|^storage.hostname=.*$|storage_hostname=$CASSANDRA_HOSTNAME|" $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
+sed -i -e "s|^index.search.hostname=.*$|index.search.hostname= $ELASTICSEARCH_HOSTNAME|" $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
+
+echo "gremlin.graph=org.janusgraph.core.JanusGraphFactory" >> $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
+
+cp $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties $JANUSGRAPH_HOME/conf/gremlin-server/rest-janusgraph-cassandra-es.properties
+
+sed -i -e "s|^host: .*$|host: 0.0.0.0|" $JANUSGRAPH_HOME/conf/gremlin-server/gremlin-server.yaml
+
+cp $JANUSGRAPH_HOME/conf/gremlin-server/gremlin-server.yaml $JANUSGRAPH_HOME/conf/gremlin-server/rest-gremlin-server.yaml
+
+sed -i -e "s|^channelizer: .*$|channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer|" $JANUSGRAPH_HOME/conf/gremlin-server/rest-gremlin-server.yaml
