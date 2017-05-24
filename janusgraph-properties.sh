fb0a1e08d0162042615f36083de46e90ae760017
Subham Rakshit <developersubham@gmail.com>
2017-05-24 17:01:51 +0100

    Changed storage backed to cassandra

--- janusgraph-properties.sh
+++ janusgraph-properties.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #set janusgraph properties
-sed -i -e "s|^storage.backend=.*$|storage.backend=cassandrathrift|" $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
+sed -i -e "s|^storage.backend=.*$|storage.backend=cassandra|" $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
 sed -i -e "s|^storage.hostname=.*$|storage.hostname=cassandra|" $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
 sed -i -e "s|^index.search.hostname=.*$|index.search.hostname=elasticsearch|" $JANUSGRAPH_HOME/conf/janusgraph-cassandra-es.properties
 
@@ -15,5 +15,3 @@ cp $JANUSGRAPH_HOME/conf/gremlin-server/gremlin-server.yaml $JANUSGRAPH_HOME/con
 sed -i -e "s|^host: .*$|host: 0.0.0.0|" $JANUSGRAPH_HOME/conf/gremlin-server/rest-gremlin-server.yaml
 sed -ri "s/( graph:).*/\1 conf\/gremlin-server\/rest-janusgraph-cassandra-es.properties}/" $JANUSGRAPH_HOME/conf/gremlin-server/rest-gremlin-server.yaml
 sed -i -e "s|^channelizer: .*$|channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer|" $JANUSGRAPH_HOME/conf/gremlin-server/rest-gremlin-server.yaml
-
-
